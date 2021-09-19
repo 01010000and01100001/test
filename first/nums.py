515ee2b64427186efd3285e9a9c18b8c69eb37d4
Panda <famquan1992@gmail.com>
2021-09-19 18:51:08 +0300

    Добавлено функция  минус

--- first/nums.py
+++ first/nums.py
@@ -2,8 +2,8 @@ def plus(a,b):
     return a+b
 
 text="new text"
-def minus(a,b):
+def mnus(a,b):
     return a-b
 if __name__== '__main__':
     print (plus(2,2))
-    print(minus(4, 2))
+    print(mnus(4, 2))
